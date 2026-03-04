c884c400756058833a0cee4f0ad9df91ba7b2c8f
devHaitham481 <devHaitham481@gmail.com>
2026-03-05 00:18:13 +0100

    add routes

---
+++ src/routes/sun.routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { getSunHours } from '../controllers/sun.controller';
+
+const router = Router();
+
+router.get('/sun-hours', getSunHours);
+
+export default router;
